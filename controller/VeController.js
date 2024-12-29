211aae703d8c59feac7de037b914a1f9c01bd67a
QuocHung3 <86833306+QuocHung3@users.noreply.github.com>
2024-12-30 06:49:51 +0700

    dat ve khu hoi

--- controller/VeController.js
+++ controller/VeController.js
@@ -148,10 +148,26 @@ const addTicket = async (req, res) => {
       from: "danhnguyen.18102002@gmail.com",
       to: req.body.email,
       subject: "NHÀ XE TUẤN TRUNG",
-      text: `
+      text: !req.body.id_chuyenXeV ? `
         NHÀ XE TUẤN TRUNG
 
-        Thông tin chuyến xe của bạn:
+        Thông tin chuyến xe đi của bạn:
+        Mã chuyến xe: ${req.body.code}
+        Mã số xe: ${req.body.soXe}
+        Số ghế: ${req.body.soGheV}
+        Nơi đi: ${req.body.noiDen}
+        Nơi đến: ${req.body.noiDi}
+        Điểm đón: ${req.body.diemDonV}
+        Điểm trả: ${req.body.diemTraV}
+        Số ĐT xe: ${req.body.sdt}
+        Thanh toán: ${req.body.trangThaiThanhToan}
+        Kiểm tra thông tin đúng trước khi lên xe, chúc quý khách thượng lộ bình an!
+      ` 
+      :
+      `
+        NHÀ XE TUẤN TRUNG
+
+        Thông tin chuyến xe đi của bạn:
         Mã chuyến xe: ${req.body.code}
         Mã số xe: ${req.body.soXe}
         Số ghế: ${req.body.soGhe}
@@ -161,8 +177,20 @@ const addTicket = async (req, res) => {
         Điểm trả: ${req.body.diemTra}
         Số ĐT xe: ${req.body.sdt}
         Thanh toán: ${req.body.trangThaiThanhToan}
+
+        Thông tin chuyến xe đi của bạn:
+        Mã chuyến xe: ${req.body.code}
+        Mã số xe: ${req.body.soXeV}
+        Số ghế: ${req.body.soGhe}
+        Nơi đi: ${req.body.noiDi}
+        Nơi đến: ${req.body.noiDen}
+        Điểm đón: ${req.body.diemDon}
+        Điểm trả: ${req.body.diemTra}
+        Số ĐT xe: ${req.body.sdt}
+        Thanh toán: ${req.body.trangThaiThanhToan}
+
         Kiểm tra thông tin đúng trước khi lên xe, chúc quý khách thượng lộ bình an!
-      `,
+      ` 
     };
 
     try {
@@ -179,6 +207,61 @@ const addTicket = async (req, res) => {
     });
 
     
+    if(!req.body.id_chuyenXeV) {
+      return;
+    }
+
+    const updateSeatsV = async () => {
+      const promises = req.body.viTriChoV.map((element) => {
+        const queryVTCho = `
+          UPDATE vitricho 
+          SET TrangThaiCho = ? 
+          WHERE ViTriCho = ? AND ID_Xe = ? AND TrangThaiCho != ?
+        `;
+        return executeQuery(queryVTCho, [
+          req.body.trangThaiVe,
+          element,
+          req.body.id_xeV,
+          req.body.trangThaiVe,
+        ]);
+      });
+      return Promise.all(promises);
+    };
+
+    const seatResultsV = await updateSeatsV();
+    const failedSeatsV = seatResultsV.filter((result) => result.affectedRows === 0);
+
+    if (failedSeatsV.length > 0) {
+      return res.status(404).json({
+        message: "Lỗi cập nhật trạng thái chỗ cho một số vị trí",
+      });
+    }
+
+    const result1 = await executeQuery(queryThemVe, [
+      req.body.id_nguoiDung,
+      req.body.id_chuyenXeV,
+      req.body.trangThaiVe,
+      req.body.soGheV.length,
+      req.body.gioKHV,
+      req.body.viTriChoV.join(" "),
+      req.body.hinhThuc,
+      req.body.soTienV,
+      req.body.trangThaiThanhToan,
+      req.body.noiDen,
+      req.body.noiDi,
+      req.body.diemDonV,
+      req.body.diemTraV,
+      req.body.sdt,
+      req.body.ghiChu,
+      req.body.code,
+    ]);
+
+    if (result1.affectedRows === 0) {
+      return res.status(400).json({
+        message: "Không thể thêm vé xe",
+      });
+    }
+
     // 5. Trả kết quả thành công
     return res.status(200).json({
       message: "Thêm vé xe thành công và email đã được gửi",
